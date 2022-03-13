44332720e6e0b97b9709075c32352134a869acd5
Joshua Cherian Mathews <jcmathew@ghc71.ghc.andrew.cmu.edu>
2022-03-12 19:12:25 -0500

    correct, lots of cache misses

--- code/wireroute.cpp
+++ code/wireroute.cpp
@@ -50,8 +50,9 @@ static void show_help(const char *program_path) {
     printf("\t-i <SA_iters>\n");
 }
 
-void addwire(wire_t wire, cost_t *costs, int dim_x, int dim_y) {
+void addwire(wire_t wire, cost_t *costs, int dim_x, int dim_y, int num_of_threads) {
     // Calculate cost on new wire, p is the start point, p+1 is the end point
+    // #pragma omp parallel for num_threads(num_of_threads)
     for (int p = 0; p < 3; p++) {
         int x1 = wire.x[p];
         int y1 = wire.y[p];
@@ -78,8 +79,46 @@ void addwire(wire_t wire, cost_t *costs, int dim_x, int dim_y) {
     costs[wire.y[2]*dim_x + wire.x[2]]--;
 }
 
-void subtractwire(wire_t wire, cost_t *costs, int dim_x, int dim_y) {
+void addwire_par(wire_t wire, cost_t *costs, int dim_x, int dim_y, int num_of_threads) {
+    int lens[4];
+    lens[0] = 0;
+    for (int p = 0; p < 3; p++) {
+        lens[p+1] = lens[p];
+        lens[p+1] += std::abs(wire.x[p+1] - wire.x[p]);
+        lens[p+1] += std::abs(wire.y[p+1] - wire.y[p]);
+    }
+
+    int p;
+
+    #pragma omp parallel for num_threads(num_of_threads)
+    for (int i = 0; i < lens[3]; i++) {
+        if (i < lens[1]) p = 0;
+        else if (i < lens[2]) p = 1;
+        else p = 2;
+
+        int x1 = wire.x[p];
+        int y1 = wire.y[p];
+        int x2 = wire.x[p+1];
+        int y2 = wire.y[p+1];
+
+        int x = x1;
+        int y = y1;
+
+        if (x1 == x2) {
+            y = std::min(y1, y2) + i - lens[p];
+        } else { // y1 == y2
+            x = std::min(x1, x2) + i - lens[p];
+        }
+
+        costs[y*dim_x + x]++;
+    }
+    costs[wire.y[1]*dim_x + wire.x[1]]--;
+    costs[wire.y[2]*dim_x + wire.x[2]]--;
+}
+
+void subtractwire(wire_t wire, cost_t *costs, int dim_x, int dim_y, int num_of_threads) {
     // Calculate cost on new wire, p is the start point, p+1 is the end point
+    // #pragma omp parallel for num_threads(num_of_threads)
     for (int p = 0; p < 3; p++) {
         int x1 = wire.x[p];
         int y1 = wire.y[p];
@@ -106,7 +145,44 @@ void subtractwire(wire_t wire, cost_t *costs, int dim_x, int dim_y) {
     costs[wire.y[2]*dim_x + wire.x[2]]++;
 }
 
-std::pair<int, int> checkcost(wire_t wire, cost_t *costs, int bendx, int bendy, int dim_x, int dim_y) {
+void subtractwire_par(wire_t wire, cost_t *costs, int dim_x, int dim_y, int num_of_threads) {
+    int lens[4];
+    lens[0] = 0;
+    for (int p = 0; p < 3; p++) {
+        lens[p+1] = lens[p];
+        lens[p+1] += std::abs(wire.x[p+1] - wire.x[p]);
+        lens[p+1] += std::abs(wire.y[p+1] - wire.y[p]);
+    }
+
+    int p;
+
+    #pragma omp parallel for num_threads(num_of_threads)
+    for (int i = 0; i < lens[3]; i++) {
+        if (i < lens[1]) p = 0;
+        else if (i < lens[2]) p = 1;
+        else p = 2;
+
+        int x1 = wire.x[p];
+        int y1 = wire.y[p];
+        int x2 = wire.x[p+1];
+        int y2 = wire.y[p+1];
+
+        int x = x1;
+        int y = y1;
+
+        if (x1 == x2) {
+            y = std::min(y1, y2) + i - lens[p];
+        } else { // y1 == y2
+            x = std::min(x1, x2) + i - lens[p];
+        }
+
+        costs[y*dim_x + x]--;
+    }
+    costs[wire.y[1]*dim_x + wire.x[1]]++;
+    costs[wire.y[2]*dim_x + wire.x[2]]++;
+}
+
+std::pair<int, int> checkcost(wire_t wire, cost_t *costs, int bendx, int bendy, int dim_x, int dim_y, int num_of_threads) {
     // wire_t test_wire;
 
     // Create new wire with potential bends
@@ -126,6 +202,7 @@ std::pair<int, int> checkcost(wire_t wire, cost_t *costs, int bendx, int bendy, 
     }
 
     // Calculate cost on new wire, p is the start point, p+1 is the end point
+    // #pragma omp parallel for num_threads(num_of_threads) reduction(max : max_cost)
     for (int p = 0; p < 3; p++) {
         int x1 = wire.x[p];
         int y1 = wire.y[p];
@@ -158,7 +235,51 @@ std::pair<int, int> checkcost(wire_t wire, cost_t *costs, int bendx, int bendy, 
     return std::pair<int, int>(max_cost, cost);
 }
 
-void serial(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y, double SA_prob) {
+std::pair<int, int> checkcost_par(wire_t wire, cost_t *costs, int bendx, int bendy, int dim_x, int dim_y, int num_of_threads) {
+    int cost = 0, max_cost = 0;
+
+    int lens[4];
+    lens[0] = 0;
+    for (int p = 0; p < 3; p++) {
+        lens[p+1] = lens[p];
+        lens[p+1] += std::abs(wire.x[p+1] - wire.x[p]);
+        lens[p+1] += std::abs(wire.y[p+1] - wire.y[p]);
+    }
+
+    int p;
+
+    #pragma omp parallel for num_threads(num_of_threads) reduction(max : max_cost)
+    for (int i = 0; i < lens[3]; i++) {
+        if (i < lens[1]) p = 0;
+        else if (i < lens[2]) p = 1;
+        else p = 2;
+
+        int x1 = wire.x[p];
+        int y1 = wire.y[p];
+        int x2 = wire.x[p+1];
+        int y2 = wire.y[p+1];
+
+        int x = x1;
+        int y = y1;
+
+        if (x1 == x2) {
+            y = std::min(y1, y2) + i - lens[p];
+        } else { // y1 == y2
+            x = std::min(x1, x2) + i - lens[p];
+        }
+
+        if (costs[y*dim_x + x] > max_cost)
+            max_cost = costs[y*dim_x + x];
+        cost += costs[y*dim_x + x];
+    }
+
+    cost -= costs[wire.y[1]*dim_x + wire.x[1]];
+    cost -= costs[wire.y[2]*dim_x + wire.x[2]];
+    
+    return std::pair<int, int>(max_cost, cost);
+}
+
+void serial(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y, double SA_prob, int num_of_threads) {
     
     // Iterate over wires
     for (int i = 0; i < num_of_wires; i++) {
@@ -167,7 +288,7 @@ void serial(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y
         int bendx = wires[i].x[1];
         int bendy = wires[i].y[1];
 
-        subtractwire(wires[i], costs, dim_x, dim_y);
+        subtractwire(wires[i], costs, dim_x, dim_y, num_of_threads);
 
         int x_start = std::min(wires[i].x[0], wires[i].x[3]);
         int x_end = std::max(wires[i].x[0], wires[i].x[3]);
@@ -192,7 +313,7 @@ void serial(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y
         } else {
             // Iterate horizontally
             for (int x = x_start; x < x_end; x++) {
-                std::pair<int, int> result = checkcost(wires[i], costs, x, wires[i].y[0], dim_x, dim_y);
+                std::pair<int, int> result = checkcost(wires[i], costs, x, wires[i].y[0], dim_x, dim_y, num_of_threads);
                 if (min_max_cost > result.first
                         || (min_sum_cost > result.second && min_max_cost == result.first)) {
                     
@@ -205,7 +326,7 @@ void serial(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y
 
             // Iterate vertically
             for (int y = y_start; y < y_end; y++) {
-                std::pair<int, int> result = checkcost(wires[i], costs, wires[i].x[0], y, dim_x, dim_y);
+                std::pair<int, int> result = checkcost(wires[i], costs, wires[i].x[0], y, dim_x, dim_y, num_of_threads);
                 if (min_max_cost > result.first
                         || (min_sum_cost > result.second && min_max_cost == result.first)) {
                     
@@ -227,7 +348,7 @@ void serial(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y
             wires[i].y[2] = wires[i].y[3];
         }
 
-        addwire(wires[i], costs, dim_x, dim_y);
+        addwire(wires[i], costs, dim_x, dim_y, num_of_threads);
     }
     
 }
@@ -235,14 +356,14 @@ void serial(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y
 void parallel(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim_y, double SA_prob, int num_of_threads) {
     
     // Iterate over wires
-    #pragma omp parallel for num_threads(num_of_threads)
+    // #pragma omp parallel for num_threads(num_of_threads)
     for (int i = 0; i < num_of_wires; i++) {
         int min_max_cost = INT_MAX;
         int min_sum_cost = INT_MAX;
         int bendx = wires[i].x[1];
         int bendy = wires[i].y[1];
 
-        subtractwire(wires[i], costs, dim_x, dim_y);
+        subtractwire(wires[i], costs, dim_x, dim_y, num_of_threads);
 
         int x_start = std::min(wires[i].x[0], wires[i].x[3]);
         int x_end = std::max(wires[i].x[0], wires[i].x[3]);
@@ -268,7 +389,7 @@ void parallel(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim
             // Iterate horizontally
             #pragma omp parallel for num_threads(num_of_threads)
             for (int x = x_start; x < x_end; x++) {
-                std::pair<int, int> result = checkcost(wires[i], costs, x, wires[i].y[0], dim_x, dim_y);
+                std::pair<int, int> result = checkcost(wires[i], costs, x, wires[i].y[0], dim_x, dim_y, num_of_threads);
                 if (min_max_cost > result.first
                         || (min_sum_cost > result.second && min_max_cost == result.first)) {
                     
@@ -282,7 +403,7 @@ void parallel(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim
             // Iterate vertically
             #pragma omp parallel for num_threads(num_of_threads)
             for (int y = y_start; y < y_end; y++) {
-                std::pair<int, int> result = checkcost(wires[i], costs, wires[i].x[0], y, dim_x, dim_y);
+                std::pair<int, int> result = checkcost(wires[i], costs, wires[i].x[0], y, dim_x, dim_y, num_of_threads);
                 if (min_max_cost > result.first
                         || (min_sum_cost > result.second && min_max_cost == result.first)) {
                     
@@ -304,7 +425,7 @@ void parallel(wire_t *wires, cost_t *costs, int num_of_wires, int dim_x, int dim
             wires[i].y[2] = wires[i].y[3];
         }
 
-        addwire(wires[i], costs, dim_x, dim_y);
+        addwire(wires[i], costs, dim_x, dim_y, num_of_threads);
     }
     
 }
@@ -378,11 +499,12 @@ int main(int argc, const char *argv[]) {
         wires[i].x[2] = wires[i].x[3];
         wires[i].y[2] = wires[i].y[3];
 
-        addwire(wires[i], costs, dim_x, dim_y);
+        addwire(wires[i], costs, dim_x, dim_y, num_of_threads);
     }
 
     init_time += duration_cast<dsec>(Clock::now() - init_start).count();
     printf("Initialization Time: %lf.\n", init_time);
+    // omp_set_nested(1);
 
     auto compute_start = Clock::now();
     double compute_time = 0;
@@ -421,6 +543,8 @@ int main(int argc, const char *argv[]) {
     fprintf(fpwires, "%d %d\n%d\n", dim_x, dim_y, num_of_wires);
     for (int i = 0; i < num_of_wires ; i++) {
         for (int p = 0; p < 4; p++) {
+            if (p > 0 && wires[i].x[p] == wires[i].x[p-1] && wires[i].y[p] == wires[i].y[p-1])
+                continue;
             fprintf(fpwires, "%d %d ", wires[i].x[p], wires[i].y[p]);
         }
         fprintf(fpwires, "\n");
